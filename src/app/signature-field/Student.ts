8e0fe19cbd15b6e7c15aba1ebb34cc5785b6b0cb
aanirudhraj <anirudhraj123@gmail.com>
2017-02-05 19:47:00 -0500

    First Commit

--- src/app/signature-field/Student.ts
+++ src/app/signature-field/Student.ts
@@ -2,8 +2,7 @@ export class Student {
   constructor(
     public name: string,
     public address: string,
-     public SSN: number,
-     public Email: string,
-     public Signature: string
+     public email: string,
+     public sig: string
   ) {  }
 }
--- src/app/signature-field/signature-field.component.ts
+++ src/app/signature-field/signature-field.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild, forwardRef }        from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { SignaturePad }                            from 'angular2-signaturepad/signature-pad';
 import { Student } from './Student';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'signature-field',
@@ -19,8 +22,9 @@ import { Student } from './Student';
 export class SignatureFieldComponent implements ControlValueAccessor {
   @ViewChild(SignaturePad) public signaturePad: SignaturePad;
 
+  constructor (private http: Http) {}
  
-  model = new Student('', '',0, '','');
+  model = new Student('', '', '','');
   submitted = false;
 
   public options: Object = {};
@@ -32,6 +36,10 @@ export class SignatureFieldComponent implements ControlValueAccessor {
   get signature(): any {
     return this._signature;
   }
+
+    private commentsUrl = 'http://192.168.0.78:8080/api/signature';
+
+     postData:string;
 
   set signature(value: any) {
     this._signature = value;
@@ -69,17 +77,38 @@ export class SignatureFieldComponent implements ControlValueAccessor {
 
   onSubmit() { 
   		this.submitted = true; 
+
+               this.postNew(this.model).subscribe(data => this.postData = JSON.stringify(data)),
+       error => console.log(error),
+       ()=>console.log("Post finished");
+
   }
   active = true;
   newStudent() {
-    this.model.Signature = '';
-     this.model = new Student('', '',0, '','');
+    this.model.sig = '';
+     this.model = new Student('', '', '','');
      this.active = false;
      setTimeout(() => this.active = true, 0);
      return JSON.stringify(this.model); 
   }
+
+postNew (body: Object): Observable<Student[]> {
+      console.log("body:"+ body);
+        let bodyString = JSON.stringify(body); // Stringify payload
+        console.log("bodyString:"+ bodyString);
+        let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        let options = new RequestOptions({ headers: headers }); // Create a request option
+
+
+
+        return this.http.post(this.commentsUrl, body, options) // ...using post request
+                         .map((res:Response) => res.json().do(console.log("resp:"+ res))) // ...and calling .json() on the response to return data
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+    }
+
+
   get diagnostic() { 
-    this.model.Signature = this.signature;
+    this.model.sig = this.signature;
   		return JSON.stringify(this.model); 
     
   }
